bec0d0b5693170c6b0e04cfd0677866aeabc586e
Faisal Ridwan <faisalridwansiregar@gmail.com>
2020-01-02 15:50:21 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/blocs/weather_bloc.dart';
+import 'package:weather_app/pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(builder: (_) => WeatherBloc()),
+
+      ],
+      child: MaterialApp(
+        title: 'State Management',
+        theme: ThemeData(
+          primaryColor: Colors.red,
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
